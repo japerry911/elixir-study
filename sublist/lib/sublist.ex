5c6a3626edd42839b343e4325b12bd435a4f46d5
Jack P <jack.e.perry@outlook.com>
2023-11-04 15:17:45 -0600

    👌 IMPROVE: added sublist exercise

--- sublist/lib/sublist.ex
+++ sublist/lib/sublist.ex
@@ -1,6 +1,15 @@
 defmodule Sublist do
   @moduledoc """
-  Module Documentation...
+  Given any two lists `A` and `B`, determine if:
+
+  - List `A` is equal to list `B`; or
+  - List `A` contains list `B` (`A` is a superlist of `B`); or
+  - List `A` is contained by list `B` (`A` is a sublist of `B`); or
+  - None of the above is true, thus lists `A` and `B` are unequal
+
+  Specifically, list `A` is equal to list `B` if both lists have the same values in the same order.
+  List `A` is a superlist of `B` if `A` contains a sub-sequence of values equal to `B`.
+  List `A` is a sublist of `B` if `B` contains a sub-sequence of values equal to `A`.
   """
 
   @type compare_result :: :equal | :superlist | :sublist | :unequal
@@ -16,11 +25,27 @@ defmodule Sublist do
   def compare(a, b) do
     cond do
       a === b -> :equal
-      is_sublist(a, b) or is_sublist(b, a) -> :sublist
+      sublist?(a, b) -> :sublist
+      superlist?(a, b) -> :superlist
       a !== b -> :unequal
     end
   end
 
-  @spec is_sublist(x :: list(), y :: list()) :: boolean()
-  defp is_sublist(x, y), do: Enum.all?(x, fn x2 -> x2 in y end)
+  @doc """
+  Returns whether x is a sublist of y.
+  """
+  @spec sublist?(x :: list(), y :: list()) :: boolean()
+  def sublist?(_x, []), do: false
+  def sublist?(x, y) do
+    case List.starts_with?(y, x) do
+      true -> true
+      false -> sublist?(x, tl(y))
+    end
+  end
+
+  @doc """
+  Returns whether x is a superlist of y.
+  """
+  @spec superlist?(x :: list(), y :: list()) :: boolean()
+  def superlist?(x, y), do: sublist?(y, x)
 end
