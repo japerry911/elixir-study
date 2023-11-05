1cc33e9d9af6ea0e6c342482990ee3e2621c53a7
Jack P <jack.e.perry@outlook.com>
2023-11-05 08:34:23 -0700

    👌 IMPROVE: some exercises completed'

--- roman-numerals/lib/roman_numerals.ex
+++ roman-numerals/lib/roman_numerals.ex
@@ -1,8 +1,24 @@
 defmodule RomanNumerals do
+  @moduledoc """
+  Module Documentation for RomanNumerals.
+  """
+
+  @roman_numerals [
+    {"M", 1000}, {"CM", 900}, {"D", 500}, {"CD", 400},
+    {"C", 100}, {"XC", 90}, {"L", 50}, {"XL", 40},
+    {"X", 10}, {"IX", 9}, {"V", 5}, {"IV", 4}, {"I", 1}
+  ]
+
   @doc """
   Convert the number to a roman number.
   """
   @spec numeral(pos_integer) :: String.t()
-  def numeral(number) do
+  def numeral(number), do: from_integer(number)
+
+  @spec from_integer(n :: non_neg_integer()) :: String.t()
+  defp from_integer(0), do: ""
+  defp from_integer(n) when n > 0 do
+    {numeral, value} = Enum.find(@roman_numerals, fn {_, v} -> v <= n end)
+    numeral <> from_integer(n - value)
   end
 end
